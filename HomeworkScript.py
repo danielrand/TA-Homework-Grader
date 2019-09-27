cbd5763006cd45c27ac8d57b8afa9836afb6833e
Daniel Rand <Daniel.Rand56@qmail.cuny.edu>
2019-09-27 13:47:54 -0400

    Added date and time of submission tracking

--- HomeworkScript.py
+++ HomeworkScript.py
@@ -1,33 +1,31 @@
 import imaplib
-import base64
 import os
 import email
-from email.parser import HeaderParser
 
 def get_mostnew_email(messages):
     # Getting in most recent emails
     ids = messages[0]  # data is a list.
     id_list = ids.split()  # ids is a space separated string
-    #latest_ten_email_id = id_list  # get all
-    latest_ten_email_id = id_list[-75:]  # get the latest 10
+    # latest_ten_email_id = id_list  # get all
+    latest_ten_email_id = id_list[-100:]  # get the latest 100
     keys = map(int, latest_ten_email_id)
     news_keys = sorted(keys, reverse=True)
     str_keys = [str(e) for e in news_keys]
-    return  str_keys
+    return str_keys
 
-username = raw_input ('Username: ')
-password = raw_input ('Password: ')
-folder = raw_input ('Path of Dirctory to save the projects in: ')
-projectInput = raw_input ('Search string for project: ')
+username = raw_input('Username: ')
+password = raw_input('Password: ')
+folder = raw_input('Path of Dirctory to save the projects in: ')
+projectInput = raw_input('Search string for project: ')
 
-mail = imaplib.IMAP4_SSL("imap.gmail.com",993)
-mail.login(username,password)
+mail = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+mail.login(username, password)
 mail.select()
 mail.select()
-(retcode, messages) =mail.search(None, 'ALL')
+(retcode, messages) = mail.search(None, 'ALL')
 news_mail = get_mostnew_email(messages)
 
-for i in news_mail :
+for i in news_mail:
     typ, data = mail.fetch(i, '(RFC822)')
     raw_email = data[0][1]
     raw_email_string = raw_email.decode('utf-8')
@@ -48,8 +46,9 @@ for i in news_mail :
         fileName = part.get_filename()
         if bool(fileName):
             filePath = os.path.join(folder, fileName)
-            if not os.path.isfile(filePath) :
+            if not os.path.isfile(filePath):
                 fp = open(filePath, 'wb')
                 fp.write(part.get_payload(decode=True))
                 fp.close()
-            print('Downloaded "{file}"'.format(file=fileName))
+            print('Downloaded "{file} received on "{date}"'.format(file=fileName, date = email_message['Date']))
+
